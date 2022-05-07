7aba9bea641cfe730290a9b4e635e5f2213c49e7
unknown <Administrator@LTP529TA.lccgroup.com>
2022-05-07 13:23:31 +0800

    First Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use App\Models\Contact;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        
+        Company::factory()->count(10)->create();
+        Contact::factory()->count(50)->create();
+    }
+}
